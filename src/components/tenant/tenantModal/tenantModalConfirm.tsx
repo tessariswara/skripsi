ba7222942215e3d2d22a9d4e1cc534314f833780
Muhammad Iqbal Ramadhan <99053601+iqbalrmadhn@users.noreply.github.com>
2024-01-12 23:16:54 +0700

    Update tenantModalConfirm.tsx

--- src/components/tenant/tenantModal/tenantModalConfirm.tsx
+++ src/components/tenant/tenantModal/tenantModalConfirm.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import TenantEditModal from './tenantEditModal';
+import AddTenant from '../tenantApi/addTenant';
+import UpdateTenant from '../tenantApi/updateTenant';
+import { deleteTenantApi } from '../tenantApi/deleteTenant';
 
 interface ModalConfirmProps {
   show: boolean;
@@ -7,7 +10,79 @@ interface ModalConfirmProps {
   isDelete: boolean;
 }
 
-const TenantModalConfirm: React.FC<ModalConfirmProps> = ({ show, handleConfirmation, isDelete }) => {
+const TenantModalConfirm: React.FC<ModalConfirmProps> = ({
+   titleButton,
+   show,
+   handleConfirmation,
+   isDelete,
+   apiPost,
+   apiDelete,
+   tenanData
+    }) => {
+   const [flagDelete, setFlagDelete] = useState(true);
+   const [save, setSave] = useState(true);
+   const [cancle, setCancle] = useState(true);
+
+   const clickDelete = (e) => {
+    e.preventDefault();
+    setFlagDelete(!flagDelete);
+    setCancle(!cancle);
+   };
+
+   const clickSave = (e) => {
+    e.preventDefault();
+    setSave(!save);
+    setCancle(!cancle);
+   };
+
+   const clickCancle = (e) => {
+    e.preventDefault();
+    setCancle(!cancle);
+   };
+
+   useEffect(() => {
+    const crudDevice = async () => {
+     if (!isDelete && !flagDelete) {
+     console.log('Data deleted successfully!');
+      setFlagDelete(!flagDelete);
+      deleteTenantApi(apiDelete, tenanData.address, [tenanData.plantName])
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('Error updating device:', error);
+      });
+      //handleConfirmation(true);
+     } else if (titleButton === "Update" && !save) {
+      console.log('Data updated successfully!');
+      setSave(!save);
+      UpdateTenant(apiPost + '/' + tenanData.plantName, tenanData.plantName, tenanData.address, tenanData.description)
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('Error updating device:', error);
+      });
+      //handleConfirmation(true);
+     } else if (titleButton === "Save" && !save) {
+      console.log('Data saved successfully!');
+      setSave(!save);
+      AddTenant(apiPost, tenanData.plantName, tenanData.address, tenanData.description)
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('Error updating device:', error);
+      });
+      //handleConfirmation(true);
+     } else {
+      console.log('Operation canceled.');
+      handleConfirmation();
+     }
+    };
+
+    crudDevice();
+   }, [cancle]);
 
   return (
     <div className={`modal ${show ? 'visible' : 'hidden'}`}>
@@ -23,15 +98,15 @@ const TenantModalConfirm: React.FC<ModalConfirmProps> = ({ show, handleConfirmat
           )}
         </div>
         <div className="modal-cta confirm">
-          <button className="button cancel" onClick={() => handleConfirmation(false)}>
+          <button className="button cancel" onClick={clickCancle}>
             Cancel
           </button>
           {isDelete ? (
-              <button className="button" onClick={() => handleConfirmation(true)}>
+              <button className="button" onClick={clickSave}>
                 Save
               </button>
           ):(
-            <button className="button" onClick={() => handleConfirmation(true)}>
+            <button className="button" onClick={clickDelete}>
               Delete
             </button>
           )}
